0a58c4851b0625f7ddac7cf59f17930bae7afe30
velikhanov <teymurvk@mail.ru>
2021-01-23 16:07:48 +0400

    Bug fixes

--- app/Http/Controllers/user/PostController.php
+++ app/Http/Controllers/user/PostController.php
@@ -43,7 +43,7 @@ class PostController extends Controller
     {
       $request->validate([
             'title' => 'required|unique:posts|max:255',
-            'category' => 'required|unique:categories,category'.$request->category,
+            'category' => 'required',
             'content' => 'required',
         ]);
 
