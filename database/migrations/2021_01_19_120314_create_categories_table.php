2b3897a9aa6cd5e902b6a1369da69b87cbefb23c
velikhanov <teymurvk@mail.ru>
2021-01-23 16:14:27 +0400

    Bug fixes

--- database/migrations/2021_01_19_120314_create_categories_table.php
+++ database/migrations/2021_01_19_120314_create_categories_table.php
@@ -18,6 +18,7 @@ class CreateCategoriesTable extends Migration
             $table->string('name');
             $table->unsignedInteger('user_id');
             $table->string('code');
+            $table->string('img');
             $table->timestamps();
         });
     }
